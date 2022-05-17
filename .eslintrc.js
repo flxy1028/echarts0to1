fba7f11c3c89ffe0eb79b128bad4a77543a0636a
fangliang@mgtv.com <fangliang@mgtv.com>
2022-05-17 11:43:46 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'import/prefer-default-export': 0,
+    'linebreak-style': ['off', 'windows'],
+  },
+  ignorePatterns: ['dist', 'esm', 'lib'],
+};
